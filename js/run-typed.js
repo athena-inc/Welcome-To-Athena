9fd437840d94f23bf6deb4c3e7f300a5f5f5207b
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2019-10-03 01:43:36 +0530

    Update run-typed.js

---
+++ js/run-typed.js
@@ -0,0 +1,11 @@
+ var typed = new Typed('#typed', {
+     stringsElement: '#typed-strings',
+     typeSpeed: 20,
+     backSpeed: 20,
+     startDelay: 1000,
+     // loop: false,
+     loop: true,
+     smartBackspace: true,
+     loopCount: Infinity,
+     cursorChar: '_',
+ });
